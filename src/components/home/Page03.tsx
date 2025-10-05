3573beeeb7b3649e739529bf27c26a6a41380e7e
yeonsuBaek <soothbugger@gmail.com>
2025-10-06 06:58:10 +0900

    style: 3페이지 모바일 스타일 적용

--- src/components/home/Page03.tsx
+++ src/components/home/Page03.tsx
@@ -1,22 +1,27 @@
 'use client'
 import React from 'react'
 import Image01 from '@/assets/images/home/page03/메인3_1.png'
+import Image02 from '@/assets/images/home/page03/메인3_2.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import { ArrowRight } from '@phosphor-icons/react'
 import { isMobileScreen } from '@/utils/common'
+import style from '@/style/components/home/Page03.module.css'
 
 function Page03() {
   const isMobile = isMobileScreen()
 
   return (
-    <section>
-      <Link href="/" aria-label="카카오페이지 보도자료 페이지로 이동하기">
-        <Image src={Image01} alt="100일만에 100만 계좌 돌파! 카카오페이에서 제공하는 서비스입니다." />
+    <section className={style.section}>
+      <Link href="/" aria-label="카카오페이지 보도자료 페이지로 이동하기" className={style.imageButton}>
+        <Image
+          src={isMobile ? Image02 : Image01}
+          alt="100일만에 100만 계좌 돌파! 카카오페이에서 제공하는 서비스입니다."
+        />
       </Link>
-      <p>카카오페이 주식, 아직 안 써보셨어요?</p>
-      <Link href="/" aria-label="카카오페이지 보도자료 페이지로 이동하기">
-        <ArrowRight size={isMobile ? 14 : 24} color="#999" aria-hidden />
+      <p className={style.text}>카카오페이 주식, 아직 안 써보셨어요?</p>
+      <Link href="/" aria-label="카카오페이지 보도자료 페이지로 이동하기" className={style.linkButton}>
+        <ArrowRight size={isMobile ? 14 : 24} color="#000" aria-hidden />
       </Link>
     </section>
   )
